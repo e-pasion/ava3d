aa4a5c64495a5763a9a32b2232489bb3008b9a38
Elias Pasion <elian.pbh@gmail.com>
2023-05-14 16:01:01 -0500

    Update crud-service.service.ts

--- cliente/src/app/services/crud-service.service.ts
+++ cliente/src/app/services/crud-service.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class CrudServiceService {
+  url='http://localhost:4000/api/'
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  obtenerTodos(variable:String): Observable<any>{
+    return this.http.get(this.url+variable+'/');
+  }
+  obtenerUno(variable:String,id:string): Observable<any>{
+    return this.http.get(this.url+variable+'/'+id);
+  }
+  guardar(objeto:any,variable:string):Observable<any>{
+    return this.http.post(this.url+variable+'/',objeto);
+  }
+  actualizar(objeto:any,variable:string,id:string):Observable<any>{
+    return this.http.put(this.url+variable+'/'+id,objeto)
+  }
+  eliminar(id:string,variable:string):Observable<any>{
+    return this.http.delete(this.url+variable+'/'+id)
+  }
 }
